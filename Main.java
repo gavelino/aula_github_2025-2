b9d902c7b9fe169f6335d97581ddf0828cd10478
Guilherme Avelino <gavelino@gmail.com>
2025-10-07 09:08:01 -0300

    reimplement issue #31

--- Main.java
+++ Main.java
@@ -4,7 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
+		int op = mainMenu.getSelection();
+
+		while (op!= 4) {
+			
+			System.out.println("\n"+ op + "foi selecionada");	
+			op = mainMenu.getSelection();
+
+		}
 		System.out.println("Fim");
 	}
 
